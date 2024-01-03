9a2b88e7a392e616b3b7864be4147a6084b35f08
kanghaeven <meerhae@gmail.com>
2024-01-03 09:59:42 +0900

    🚀Feat : 404 페이지 생성

---
+++ ourhome-shop/src/pages/Error404Page.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const StartDiv = styled.div`
+  color: black;
+`;
+
+function Error404Page() {
+  return (
+    <StartDiv>
+      <StartDiv>Error404</StartDiv>
+      <StartDiv>Error404</StartDiv>
+    </StartDiv>
+  );
+}
+
+export default Error404Page;
