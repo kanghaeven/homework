42c2f2114e7f3c5897c1e44342676e06d50548b9
kanghaeven <meerhae@gmail.com>
2024-01-03 20:52:20 +0900

    🚑Fix: 상품 리스트에서 장바구니 담는 로직 수정

--- ourhome-shop/src/contexts/CartContext.tsx
+++ ourhome-shop/src/contexts/CartContext.tsx
@@ -1,17 +1,17 @@
 import { ReactNode, createContext, useState } from "react";
-import { CartContextType } from "~/types/cart";
+import { useNavigate } from "react-router-dom";
+import { TCartContext } from "~/types/cart";
 import { TShopItem } from "~/types/list";
 
-// Context 생성
-export const CartContext = createContext<CartContextType | null>({
+export const CartContext = createContext<TCartContext | null>({
   cart: [],
   addToCart: () => {},
-  removeFromCart: () => {},
   setCart: () => {},
 });
 
 // Context Provider 컴포넌트 생성
 export const CartProvider = ({ children }: { children: ReactNode }) => {
+  const navigate = useNavigate();
   const [cart, setCart] = useState<TShopItem[]>([]);
 
   const addToCart = (item: TShopItem) => {
@@ -24,7 +24,8 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
           cartItem.id === item.id
             ? {
                 ...cartItem,
-                quantity: cartItem.itemQuantity + 1,
+                itemQuantity:
+                  (cartItem.itemQuantity ? cartItem.itemQuantity : 0) + 1,
               }
             : cartItem
         )
@@ -33,14 +34,17 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
       // 카트에 없는 경우 새로운 아이템 추가
       setCart([...cart, { ...item, itemQuantity: 1 }]);
     }
-  };
 
-  const removeFromCart = (id: number) => {
-    setCart(cart.filter((item: TShopItem) => item.id !== id));
+    const confirmed = window.confirm(
+      `${item.itemName} 상품을 담았습니다. 장바구니로 이동하시겠습니까?`
+    );
+    if (confirmed) {
+      navigate("/cart");
+    }
   };
 
   return (
-    <CartContext.Provider value={{ cart, addToCart, removeFromCart, setCart }}>
+    <CartContext.Provider value={{ cart, addToCart, setCart }}>
       {children}
     </CartContext.Provider>
   );
